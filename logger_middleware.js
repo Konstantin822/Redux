cd5fbc1a1553d4abf6e2a758e2b64fa3f1996258
Konstantin <bladflag8@gmail.com>
2025-03-17 11:32:11 +0200

    logger middleware

---
+++ logger_middleware.js
@@ -0,0 +1,66 @@
+const redux = require('redux')
+const createStore = redux.createStore
+const combineReducers = redux.combineReducers
+const reduxLogger = require('redux-logger')
+const logger = reduxLogger.createLogger()
+const applyMiddleware = redux.applyMiddleware
+
+const ORDER_PIZZA = 'ORDER_PIZZA'
+const ORDER_BURGER = 'ORDER_BURGER'
+
+function orderPizza() {
+    return {
+        type: ORDER_PIZZA,
+    }
+}
+
+function orderBurger() {
+    return {
+        type: ORDER_BURGER,
+    }
+}
+
+const initialStatePizza = {
+    pizzaBase: 100,
+}
+const initialStateBurger = {
+    burgerBuns: 200
+}
+
+const reducerPizza = (prevState = initialStatePizza, action) => {
+    switch (action.type) {
+        case ORDER_PIZZA:
+            return {
+                ...prevState,
+                pizzaBase: prevState.pizzaBase - 1
+            }
+        default:
+            return prevState
+    }
+}
+const reducerBurger = (prevState = initialStateBurger, action) => {
+    switch (action.type) {
+        case ORDER_BURGER:
+            return {
+                ...prevState,
+                burgerBuns: prevState.burgerBuns - 1
+            }
+        default:
+            return prevState
+    }
+}
+
+const rootReducer = combineReducers({
+    pizza: reducerPizza,
+    burger: reducerBurger
+})
+const store = createStore(rootReducer, applyMiddleware(logger))
+console.log('Initial State', store.getState())
+
+const unsubscribe = store.subscribe(() => { })
+
+store.dispatch(orderPizza())
+store.dispatch(orderPizza())
+store.dispatch(orderBurger())
+store.dispatch(orderBurger())
+unsubscribe()
